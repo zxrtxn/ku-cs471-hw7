69e16cde4b6c8cbf56e18108fb53274536debbbd
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-08-26 22:03:41 +0700

    implement getAllRestaurants method

--- src/main/java/ku/cs/restaurant/controller/RestaurantController.java
+++ src/main/java/ku/cs/restaurant/controller/RestaurantController.java
@@ -1,13 +1,21 @@
 package ku.cs.restaurant.controller;
 
+import ku.cs.restaurant.entity.Restaurant;
+import ku.cs.restaurant.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RestaurantController {
 
+    @Autowired
+    private RestaurantService service;
+
     @GetMapping("/restaurant")
-    public String getAllRestaurants() {
-        return "All Restaurants";
+    public List<Restaurant> getAllRestaurants() {
+        return service.getAll();
     }
 }
