bcf46894c0ee0e58fe1396a47e34f725df91ed2d
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-08-26 21:38:52 +0700

    first commit

---
+++ src/main/java/ku/cs/restaurant/KuCs471Hw7Application.java
@@ -0,0 +1,13 @@
+package ku.cs.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuCs471Hw7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuCs471Hw7Application.class, args);
+	}
+
+}
---
+++ src/test/java/ku/cs/restaurant/KuCs471Hw7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cs.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuCs471Hw7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
