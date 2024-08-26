bdb1a0720785c4966767262d0e670d7ccb135bf5
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-08-26 21:59:59 +0700

    add Restaurant repository

---
+++ src/main/java/ku/cs/restaurant/repository/RestaurantRepository.java
@@ -0,0 +1,11 @@
+package ku.cs.restaurant.repository;
+
+import ku.cs.restaurant.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, UUID> {
+}
